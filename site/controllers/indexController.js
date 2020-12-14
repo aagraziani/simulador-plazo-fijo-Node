7ac3082d6d57839fd48fa443ac739205187df9d7
aagraziani <aagraziani@gmail.com>
2020-12-14 16:16:02 -0300

    Segundo commit

--- site/controllers/indexController.js
+++ site/controllers/indexController.js
@@ -1,23 +1,10 @@
-const fetch = require('node-fetch');
 
 let controlador = {
 
     index: function (req, res) {
-        /*
-         fetch('http://localhost:3000/api/bancosTasas')
-
-            .then(function(response){
-                return response.json();
-            })
-
-            .then(function(jsonResponse){*/
-                return res.render('index'/*, { jsonResponse: jsonResponse.data }*/);
-            /*})
-
-            .catch(function(error){
-                console.error(error);
-            })
-            */
+        
+                return res.render('index');
+            
         }
 };
 
